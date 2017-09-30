6a7a1204a184a1107c72c69b52a9a95e32abd18b
Methusael Murmu <blendit07@gmail.com>
2017-09-30 19:09:40 +0530

    Decoupled data from layout

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
+const dataPath = 'data';
 const port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+var profile_obj = JSON.parse(fs.readFileSync(dataPath + '/profile.json', 'utf8'));
+
 // Routes
 app.get('/', (req, res) => {
-    res.render('pages/index');
+    res.render('pages/index', profile_obj);
 });
 
 app.listen(port, () => {
